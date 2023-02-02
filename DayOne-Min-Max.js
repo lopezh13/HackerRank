7a61de561a381a74c5086913f63c242f9f172504
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-02-02 13:18:57 -0600

    Create DayOne-Min-Max.js
    Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.

---
+++ DayOne-Min-Max.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+  /*write you code*/
+    arr.sort((a, b) => a - b);
+    let min = arr.slice(0, arr.length - 1).reduce((acc, current) => acc + current, 0);
+    let max = arr.slice(1).reduce((acc, current) => acc + current, 0);
+    console.log(min, max);
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
