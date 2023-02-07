b0bdab59d0fc301e0300bba4082fa3b9cafc8c73
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-02-06 19:47:05 -0600

    Create DayFive-BalancedBrackets.js

---
+++ DayFive-BalancedBrackets.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function isBalanced(s) {
+    let stack = [];
+    let map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        } else {
+            let last = stack.pop();
+
+            if (s[i] !== map[last]) {
+                return 'NO';
+            }
+        }
+    }
+
+    if (stack.length !== 0) {
+        return 'NO';
+    }
+
+    return 'YES';
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const s = readLine();
+
+        const result = isBalanced(s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
