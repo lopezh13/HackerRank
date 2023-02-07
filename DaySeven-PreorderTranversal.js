53748f3731697e9ac742958fa9b8a66062ed0ccd
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-02-06 20:48:39 -0600

    Create DaySeven-PreorderTranversal.js

---
+++ DaySeven-PreorderTranversal.js
@@ -0,0 +1,49 @@
+var Tree = function() {
+    this.root = null;
+}
+
+Tree.prototype.insert = function(node, data) {
+    if (node == null){
+    	node = new Node(data);
+    }
+ 	else if (data < node.data){
+        node.left  = this.insert(node.left, data);
+    }
+    else{
+        node.right = this.insert(node.right, data);   
+    }
+
+    return node;
+}
+
+var Node = function(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+/* head ends */
+
+/*
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function preOrder(root) {
+    process.stdout.write((root.data).toString() + " ");
+    if (root.left){
+        preOrder(root.left);
+    }
+    if (root.right){
+        preOrder(root.right);
+    } 
+}
+
+/* t
