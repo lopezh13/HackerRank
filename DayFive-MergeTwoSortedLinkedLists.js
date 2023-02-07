d84c69ea03f2b74a49029c636fdb4b85c2d4726e
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-02-06 19:41:51 -0600

    Create DayFive-MergeTwoSortedLinkedLists.js

---
+++ DayFive-MergeTwoSortedLinkedLists.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+let dummy = new SinglyLinkedListNode(-1);
+let tail = dummy;
+while (head1 !== null && head2 !== null) {
+if (head1.data < head2.data) {
+tail.next = head1;
+head1 = head1.next;
+} else {
+tail.next = head2;
+head2 = head2.next;
+}
+tail = tail.next;
+}
+tail.next = head1 === null ? head2 : head1;
+return dummy.next;
+
+}
+
+function main() {
